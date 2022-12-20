bfb0f3895513de0119776c4aef1a58467e468fa9
reignseth87 <reignseth87@gmail.com>
2022-12-20 08:38:47 -0800

    takes point and updates calue to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * reset_to_98 - reset the integer to 98
+ * @n: a pointer the integer we want to reset to 98
+ * Return: nothing
+ */
+
+void reset_to_98(int *n)
+{
+
+	 *n = 98;
+}
