ea60d5bbcaced4bccc75dd3b5f36269b70c81c67
reignseth87 <reignseth87@gmail.com>
2023-01-16 08:04:59 -0800

    update

--- 0-object_like_macro.h
+++ 0-object_like_macro.h
@@ -1,13 +1,11 @@
+0-object_like_macro.h CODE
+
+
 #ifndef OBJECT_LIKE_MACRO_H
 #define OBJECT_LIKE_MACRO_H
-
-/*
- * File: 0-object_like_macro.h
- * Auth: Patience Wellington
- * Desc: Header file defines a macro named
- *       SIZE as an abbreviation for the token 1024.
- */
 
 #define SIZE 1024
 
 #endif
+
+
